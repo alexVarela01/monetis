967c60251f1ee7dbb3ba6a72ecfb8267d63bd924
Alexandre <nawtz_sky@hotmail.com>
2025-02-12 11:04:33 +0000

    fixed vercel json and endpoint

--- app/api/cleanup/route.ts
+++ app/api/cleanup/route.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
  
-export async function POST(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const authHeader = request.headers.get('authorization');
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response('Unauthorized', {
