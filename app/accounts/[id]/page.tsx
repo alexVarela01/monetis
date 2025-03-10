43ea5f1f495d9e65664bdcafebde859c1d1920fc
Alexandre <nawtz_sky@hotmail.com>
2025-03-10 10:17:26 +0000

    fix edit account name inout

--- app/accounts/[id]/page.tsx
+++ app/accounts/[id]/page.tsx
@@ -436,7 +436,7 @@ export default function Account({ params }: { params: Promise<{ id: string }> })
               <label htmlFor="name">Account name</label>
               <input
                 type="text"
-                name="amount"
+                name="name"
                 value={accountName}
                 maxLength={25}
                 onChange={(e) => setAccountName(e.target.value)}
